09461fd24f23d6a195e098f5942b1489789760ba
笹倉直哉 <a_20100409@yahoo.co.jp>
2024-09-17 14:11:22 +0900

    コードを修正

--- app/main.py
+++ app/main.py
@@ -3,9 +3,9 @@ from sklearn.datasets import fetch_california_housing
 
 # Populationの値によって3種類のカテゴリに区分する関数
 def category(df):
-  if df < 600: return 'few'
-  elif df > 3000: return 'many'
-  else: return 'usually'
+  if df < 600: return 'few' # 600以下はfew
+  elif df > 3000: return 'many' # 3000以上はmany
+  else: return 'usually' # 中間の値はusually
 
 housing = fetch_california_housing() # データセットを取得
 # dataキーで8項目のデータを抽出
@@ -17,13 +17,15 @@ df_housing = pd.DataFrame(
 p_describe = df_housing['Population'].describe()
 # print(p_describe)
 
+# Populationのデータをカテゴリ化
 p_category = df_housing['Population'].apply(category)
 # print(df_housing['Population'].apply(category))
 
+# Pandasのget_dummies関数でカテゴリデータに対応したダミー変数を作成
 p_dummy = pd.get_dummies(p_category, drop_first=True, dtype=int)
 # print(p_dummy)
 
-X = pd.concat([df_housing,p_dummy], axis=1) # many、usuallyを追加
-X = X.drop(['Population'], axis=1) # Populationの列を削除
-X.head() # 冒頭5件を出力
-# print(X)
+# 作成したダミー変数を元のデータに追加し、Populationの列を削除
+X = pd.concat([df_housing,p_dummy], axis=1)
+X = X.drop(['Population'], axis=1)
+print(X)
